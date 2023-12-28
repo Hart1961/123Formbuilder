40cae5cf1887fdd2a8dc2ad92d2713477b7efc7f
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2023-12-28 15:53:27 -0700

    Add files via upload

--- ForBrokenForm.js
+++ ForBrokenForm.js
@@ -2,6 +2,8 @@
 window.addEventListener('load', function(){
 	var sourceControlId = "text-00000010", /** ID OF THE SENDER CONTROL **/
 	targetControlId = "text-00000012" /** ID OF THE RECEIVER CONTROL **/
+	
+	window.alert("I got called");
 
 	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
 	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
