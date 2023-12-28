9541e082d8f4b870df287688c854f7a32fd5c8be
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2023-12-28 14:21:07 -0700

    Add files via upload

--- ForBrokenForm.js
+++ ForBrokenForm.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
 	var sourceControlId = 111041865, /** ID OF THE SENDER CONTROL **/
 	targetControlId = 111045282, /** ID OF THE RECEIVER CONTROL **/
 
-	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId),
+	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
 	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
 	sourceControlInstance.on('value-change', function(){
